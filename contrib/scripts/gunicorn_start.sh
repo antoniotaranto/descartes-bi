0ed958e0cf5f32a6786779cd8375d607deecd128
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-08-18 00:00:00 -0400

    Add contrib files

---
+++ contrib/scripts/gunicorn_start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+NAME="descartes-bi"
+DJANGODIR=/usr/share/descartes-bi
+SOCKFILE=/var/tmp/gunicorn.sock
+USER=www-data
+GROUP=www-data
+NUM_WORKERS=3
+DJANGO_SETTINGS_MODULE=descartes_bi.settings
+DJANGO_WSGI_MODULE=descartes_bi.wsgi
+TIMEOUT=600
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --log-level=debug \
+  --bind=unix:$SOCKFILE \
+  --timeout=$TIMEOUT
+
