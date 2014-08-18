c38dc1d0852f072fae09c2d7910bf7ac8a7ea1ca
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-08-17 23:30:27 -0400

    Fix binary name

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
     name=PACKAGE_NAME,
     packages=find_packages(PACKAGE_DIR),
     platforms=['any'],
-    scripts=['descartes_bi/bin/descarte-bi.py'],
+    scripts=['descartes_bi/bin/descartes-bi.py'],
     url='https://github.com/rosarior/descartes_bi',
     version=descartes_bi.__version__,
     zip_safe=False,
